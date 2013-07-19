a5c742e14854f91c3fc8b6c387abdd3607120de1
datagutten <datagutten@datagutten.net>
2013-07-19 21:45:24 +0200

    Made the page list generator a class

--- sheets.php
+++ sheets.php
@@ -1,30 +1,30 @@
 <?Php
-//First scan the front of the pages, starting from the center page
-//Then scan the back of the pages, starting from the cover page
-function pad($int)
+class sheets
 {
-return str_pad($int,2,'0',STR_PAD_LEFT);
+	//First scan the front of the pages, starting from the center page
+	//Then scan the back of the pages, starting from the cover page
+	private function pad($int)
+	{
+		return str_pad($int,2,'0',STR_PAD_LEFT);
+	}
+	
+	function __construct($sheets)
+	{
+		$sheets=$sheets;
+		$pages=$sheets*4;
+		
+		$this->inside['l'][0]=$pages/2; //The left center page is total pages/2
+		$this->inside['r'][0]=$pages/2+1; //The rigt center page is total pages/2+1
+		$this->outside['l'][0]=$pages; //The left cover page is total pages
+		$this->outside['r'][0]='01'; //The right cover page is the first page
+		
+		for ($i=1; $i<$sheets; $i++)
+		{
+				$this->inside['l'][$i]=$this->pad($this->inside['l'][$i-1]-2); //Left front
+				$this->inside['r'][$i]=$this->pad($this->inside['r'][$i-1]+2); //Right front
+				$this->outside['l'][$i]=$this->pad($this->outside['l'][$i-1]-2); //Left back
+				$this->outside['r'][$i]=$this->pad($this->outside['r'][$i-1]+2); //Right back
+		
+		}
+	}
 }
-
-
-if(!isset($sheets) && isset($pages))
-	$sheets=$pages/4; //4 pages per sheet
-elseif(isset($sheets) && !isset($pages))
-	$pages=$sheets*4;
-
-$scans['l'][0]=$pages/2; //The left center page is total pages/2
-$scans['r'][0]=$pages/2+1; //The rigt center page is total pages/2+1
-$scans2['l'][0]=$pages; //The left cover page is total pages
-$scans2['r'][0]='01'; //The right cover page is the first page
-
-
-
-for ($i=1; $i<$sheets; $i++)
-{
-$scans['l'][$i]=pad($scans['l'][$i-1]-2); //Left front
-$scans['r'][$i]=pad($scans['r'][$i-1]+2); //Right front
-$scans2['l'][$i]=pad($scans2['l'][$i-1]-2); //Left back
-$scans2['r'][$i]=pad($scans2['r'][$i-1]+2); //Right back
-
-}
-//print_r($scans);
